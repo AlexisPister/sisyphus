d9894a346d24f261a3990ec24c72c94cbe8388b6
apister <alexis.pister@inria.fr>
2023-01-24 14:23:51 +0100

    svg anim

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,7 +1,10 @@
 <script lang="ts" setup>
+/// <reference types="vite-svg-loader"/>
 import {computed, onBeforeMount, Ref, ref, toRaw} from "vue";
 import axios from "axios";
 import {getCurrentWeek, getHostname} from "../utils";
+import svgSis1 from "../assets/g37_sis.svg?component";
+import svgSis2 from "../assets/g38_sis.svg?component";
 
 // const urlsBlocked: Ref<Array<URL>> = ref([]);
 const urlsBlocked: Ref<Array<string>> = ref([]);
@@ -9,6 +12,7 @@ const newSite: Ref<string> = ref('');
 let isLoggedInTwitter: Ref<Boolean> = ref(false);
 let message: Ref<string> = ref('');
 let isWeekStarted: Ref<Boolean> = ref(false);
+let animateFlag: Ref<number> = ref(0);
 
 
 // DELETE FOR TESTS
@@ -18,7 +22,7 @@ onStart();
 
 function onStart(): void {
   getIfProductivityStarted();
-  fillUrlsFromStorage();;
+  fillUrlsFromStorage();
 }
 
 function getIfProductivityStarted(): void {
@@ -128,8 +132,21 @@ function postTweet(): void {
       })
 }
 
+function animateIcon(): void {
+  if (animateFlag.value == 0) {
+    animateFlag.value = 1
+  } else if (animateFlag.value == 1) {
+    animateFlag.value = 0
+  } else {
+    throw "Animation Error";
+  }
+}
+
+
 checkLoggedInTwitter();
 setInterval(checkLoggedInTwitter, 2000);
+setInterval(animateIcon, 500);
+
 
 </script>
 
@@ -155,6 +172,10 @@ setInterval(checkLoggedInTwitter, 2000);
     </div>
 
     <div>
+<!--      <img :src="svgSis" />-->
+      <svgSis1 class="anim-icon" v-if="animateFlag==0"></svgSis1>
+      <svgSis2 class="anim-icon" v-else></svgSis2>
+
       <p v-if="isWeekStarted">Sisyphus is walking the mountain.</p>
       <button v-else @click="startProductiveDay">Start a Productive Day</button>
     </div>
@@ -172,15 +193,17 @@ setInterval(checkLoggedInTwitter, 2000);
 #sites {
   display: flex;
   flex-direction: column;
-  column-gap: 5px;
-  width: 60vw;
+  row-gap: 3px;
+  width: 70%;
+  /*width: 50%;*/
+  /*justify-content: center;*/
+  /*text-align: center;*/
 }
 
 .site {
   border: 1px solid black;
   /*padding: 2%;*/
-  /*width: 60vw;*/
-  width: 80%;
+  /*width: 70%;*/
   /*vertical-align: middle;*/
 }
 
@@ -194,6 +217,26 @@ setInterval(checkLoggedInTwitter, 2000);
   /*line-height: 60%;*/
 }
 
+button {
+  border-radius: 8px;
+  /*border: 1px solid transparent;*/
+  border: 1px solid black;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  /*background-color: rgba(232, 121, 121, 0.67);*/
+  cursor: pointer;
+  transition: border-color 0.25s;
+}
+button:hover {
+  border-color: #646cff;
+}
+button:focus,
+button:focus-visible {
+  outline: 4px auto -webkit-focus-ring-color;
+}
+
 .close-btn {
   float: right;
   /*margin-right: 6px;*/
@@ -205,10 +248,10 @@ setInterval(checkLoggedInTwitter, 2000);
 }
 
 #options {
-  display: flex;
-  flex-direction: column;
-  column-gap: 5px;
-  width: 60vw;
+  /*display: flex;*/
+  /*flex-direction: column;*/
+  /*column-gap: 5px;*/
+  /*width: 60vw;*/
 }
 
 #input-new-site {
@@ -219,4 +262,10 @@ setInterval(checkLoggedInTwitter, 2000);
   width: 80%;
   /*padding: 2%;*/
 }
+
+.anim-icon {
+  height: 160px;
+  width: 160px;
+}
+
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { crx } from '@crxjs/vite-plugin'
+import svgLoader from 'vite-svg-loader'
 import manifest from './public/manifest.json' // Node 14 & 16
 // import manifest from './public/manifest.json' assert { type: 'json' } // Node >=17
 
@@ -8,6 +9,7 @@ import manifest from './public/manifest.json' // Node 14 & 16
 export default defineConfig({
   plugins: [
       vue(),
+      svgLoader(),
       crx({ manifest })
   ]
 })
